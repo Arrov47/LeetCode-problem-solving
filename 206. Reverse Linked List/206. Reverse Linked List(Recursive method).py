6be7b7bb876fa54d4f7b25fd26a19be0ba2427cb
Arrov47 <ar.rov47@gmail.com>
2026-04-12 18:30:49 +0500

    Optimization: 206. Reverse Linked List(Recursive method).py

--- 206. Reverse Linked List/206. Reverse Linked List(Recursive method).py
+++ 206. Reverse Linked List/206. Reverse Linked List(Recursive method).py
@@ -4,7 +4,6 @@ class ListNode(object):
         self.val = val
         self.next = next
 class Solution(object):
-    retNode = None
 
     def reverseList(self, head):
         """
@@ -12,16 +11,24 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
         
-        def recurs(node):
-            if node.next is None:
-                self.retNode = ListNode(node.val, self.retNode)
-            else:
-                self.retNode = ListNode(node.val, self.retNode)
-                recurs(node.next)
-        if head:
-            recurs(head)
-        return self.retNode
-
+        if not head or not head.next:
+            return head
+        
+        newHead = self.reverseList(head.next)
+        
+        head.next.next = head
+        head.next = None
+        
+        return newHead
+if __name__ == "__main__":
+    testCase = ListNode(1, ListNode(2, ListNode(3,ListNode(4, ListNode(5, None)))))
+    a = Solution()
+    l = list()
+    curr = testCase
+    while curr:
+        l.append(curr.val)
+        curr = curr.next
+    print(l[::-1])
             
 
 
